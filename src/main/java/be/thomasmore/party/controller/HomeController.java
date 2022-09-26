a17f3e3b9ea59a7f594aec3b8c932e8b2a22a472
WimDC <r0894616@student.thomasmore.be>
2022-09-26 21:56:23 +0200

    Party2022 first save

--- src/main/java/be/thomasmore/party/controller/HomeController.java
+++ src/main/java/be/thomasmore/party/controller/HomeController.java
@@ -1,2 +1,26 @@
-package be.thomasmore.party.controller;public class HomeController {
+package be.thomasmore.party.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class HomeController {
+    private final int mySpecialNumber = 35;
+
+    @GetMapping
+    public String home(Model model) {
+        model.addAttribute("mySpecialNumber",mySpecialNumber);
+        return "home";
+    }
+    @GetMapping("/about")
+    public String about() {
+        return "about";
+    }
+    @GetMapping("/venuedetails/{venueName}")
+    public String venueDetails(Model model, @PathVariable String venueName) {
+        model.addAttribute("venueName",venueName);
+        return "venueDetails";
+    }
 }
