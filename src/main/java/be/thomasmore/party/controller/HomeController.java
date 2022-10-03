6c03ef3a7203c6223664de266d63c1303015b5db
WimDC <r0894616@student.thomasmore.be>
2022-10-03 21:47:31 +0200

    3.2 Exercise: herhaling Pay toegevoegd klaar

--- src/main/java/be/thomasmore/party/controller/HomeController.java
+++ src/main/java/be/thomasmore/party/controller/HomeController.java
@@ -5,6 +5,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.Calendar;
+import java.util.Date;
+
 @Controller
 public class HomeController {
     private final int mySpecialNumber = 35;
@@ -28,5 +34,17 @@ public class HomeController {
     public String venueDetails(Model model, @PathVariable(required=false) String venueName) {
         model.addAttribute("venueName",venueName);
         return "venuedetails";
+    }
+    @GetMapping("/pay")
+    public String pay(Model model){
+        DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+        Date today = new Date();
+        Calendar c = Calendar.getInstance();
+        c.setTime(today);
+        model.addAttribute("today",format.format(today));
+        c.add(Calendar.DATE,5);
+        Date paydate = c.getTime();
+        model.addAttribute("paydate", format.format(paydate));
+        return "pay";
     }
 }
