da52020dd88556ba4b36d3b2984f5a1ad5994e7d
WimDC <r0894616@student.thomasmore.be>
2022-11-07 20:37:24 +0100

    4.2 Exercise: maak een nieuwe Model class Artist

--- src/main/java/be/thomasmore/party/model/Artist.java
+++ src/main/java/be/thomasmore/party/model/Artist.java
@@ -1,4 +1,61 @@
 package be.thomasmore.party.model;
 
 public class Artist {
+    private int id;
+    private String artistName;
+    private String linkMoreInfo;
+    private String genre;
+    private String bio;
+    private String portfolio;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public String getLinkMoreInfo() {
+        return linkMoreInfo;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public String getPortfolio() {
+        return portfolio;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+
+    public void setLinkMoreInfo(String linkMoreInfo) {
+        this.linkMoreInfo = linkMoreInfo;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public void setPortfolio(String portfolio) {
+        this.portfolio = portfolio;
+    }
+
+    public Artist() {
+    }
 }
