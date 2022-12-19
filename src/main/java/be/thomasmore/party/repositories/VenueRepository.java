facd001db3634d8901379625b552b87f3125380d
WimDC <r0894616@student.thomasmore.be>
2022-12-19 18:56:39 +0100

    5.6 Exercise: simpele JPQL

--- src/main/java/be/thomasmore/party/repositories/VenueRepository.java
+++ src/main/java/be/thomasmore/party/repositories/VenueRepository.java
@@ -1,12 +1,20 @@
 package be.thomasmore.party.repositories;
 
 import be.thomasmore.party.model.Venue;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
 public interface VenueRepository extends CrudRepository<Venue, Integer> {
     Iterable<Venue> findByOutdoor(boolean outdoor);
     Iterable<Venue> findByIndoor(boolean indoor);
     Iterable<Venue> findByCapacity(int capacity);
     Iterable<Venue> findByCapacityLessThan(int capacity);
+    @Query("select v from Venue v where v.capacity >= ?1")
+    List<Venue> findByCapacityGreaterThanEqualQuery(int min);
+    @Query("select v from Venue v where v.capacity between ?1 and ?2")
+    List<Venue> findByCapacityBetweenQuery(int min, int max);
     Iterable<Venue> findByCapacityBetween(int startCapacity, int endCapacity);
     Iterable<Venue> findByCapacityGreaterThanEqual(int capacity);
 }
