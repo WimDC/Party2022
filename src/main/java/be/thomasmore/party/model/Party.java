22ac9ed8ae77aa9cb7d06d9ba1ff0d2ed2819663
WimDC <r0894616@student.thomasmore.be>
2022-12-27 20:46:19 +0100

    2.1 Exercise: maak een nieuwe Entity class ‘Party’ 3.1 Party is associated met Venues 3.2 Database update
    klaar

---
+++ src/main/java/be/thomasmore/party/model/Party.java
@@ -0,0 +1,91 @@
+package be.thomasmore.party.model;
+
+import be.thomasmore.party.model.Venue;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Party {
+    @Id
+    private Integer id;
+    private String name;
+    private Integer pricePresaleInEur;
+    private Integer priceInEur;
+    private String extraInfo;
+    @Temporal(TemporalType.DATE)
+    private Date date;
+    @Temporal(TemporalType.TIME)
+    private Date doors;
+
+    @ManyToOne
+    private Venue venue;
+
+
+    public Party() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPricePresaleInEur() {
+        return pricePresaleInEur;
+    }
+
+    public void setPricePresaleInEur(Integer pricePresaleInEur) {
+        this.pricePresaleInEur = pricePresaleInEur;
+    }
+
+    public Integer getPriceInEur() {
+        return priceInEur;
+    }
+
+    public void setPriceInEur(Integer priceInEur) {
+        this.priceInEur = priceInEur;
+    }
+
+    public String getExtraInfo() {
+        return extraInfo;
+    }
+
+    public void setExtraInfo(String extraInfo) {
+        this.extraInfo = extraInfo;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Date getDoors() {
+        return doors;
+    }
+
+    public void setDoors(Date doors) {
+        this.doors = doors;
+    }
+
+    public Venue getVenue() {
+        return venue;
+    }
+
+    public void setVenue(Venue venue) {
+        this.venue = venue;
+    }
+}
