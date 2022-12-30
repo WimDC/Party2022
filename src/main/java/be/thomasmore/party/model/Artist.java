2bb1b00be6ca2ac27f9590bd87e33b61c0f417bb
WimDC <r0894616@student.thomasmore.be>
2022-12-30 14:52:28 +0100

    6.3 Exercise: show Parties for this Artist

--- src/main/java/be/thomasmore/party/model/Artist.java
+++ src/main/java/be/thomasmore/party/model/Artist.java
@@ -1,9 +1,6 @@
 package be.thomasmore.party.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
+import javax.persistence.*;
 import java.util.Collection;
 
 @Entity
@@ -16,7 +13,7 @@ public class Artist {
     @Column(length=1000)
     private String bio;
     private String portfolio;
-    @ManyToMany(mappedBy = "artists")
+    @ManyToMany(mappedBy = "artists", fetch = FetchType.LAZY)
     private Collection<Party> parties;
 
     public int getId() {
