1ad8abd3842b222b81b68f81accd0ee49e60c184
WimDC <r0894616@student.thomasmore.be>
2023-01-02 09:53:49 +0100

    2.1.1 Nieuwe Controller en View

---
+++ src/main/java/be/thomasmore/party/controller/AdminController.java
@@ -0,0 +1,32 @@
+package be.thomasmore.party.controller;
+
+import be.thomasmore.party.model.Party;
+import be.thomasmore.party.repositories.PartyRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+    private Logger logger = LoggerFactory.getLogger(AdminController.class);
+    @Autowired
+    private PartyRepository partyRepository;
+    @GetMapping("/partyedit/{id}")
+    public String partyEdit(Model model,
+                            @PathVariable Integer id) {
+        if (id == null) return "admin/partyedit";
+        Optional<Party> optionalParty = partyRepository.findById(id);
+        if (optionalParty.isPresent()) {
+            model.addAttribute("party", optionalParty.get());
+        }
+        return "admin/partyedit";
+    }
+}
