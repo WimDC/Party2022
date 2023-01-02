31c936fcdf1e90ff24be72df66611bbdb7a4cfba
WimDC <r0894616@student.thomasmore.be>
2023-01-02 20:15:59 +0100

    7.1 Client side Validation

--- src/main/java/be/thomasmore/party/controller/AdminController.java
+++ src/main/java/be/thomasmore/party/controller/AdminController.java
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 import javax.validation.Validation;
 import java.util.List;
 import java.util.Optional;
